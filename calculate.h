bfc967aba3f716aac0d635d93fb6bdb93b8368fb
bernice17824 <H34036186@mail.ncku.edu.tw>
2015-07-03 10:40:11 +0800

    lab3

---
+++ calculate.h
@@ -0,0 +1,10 @@
+#ifndef CALCULATE_H
+#define CALCULATE_H
+using namespace std;
+
+void readFile(vector<int>& weight, int vecSize);
+void reverseSort (vector<int>& weight);
+int sumOfTopFive (vector<int>& weight);
+
+#endif
+
